0febd9cf7f17aa43782f754bd7ddddacaf494e8c
LukyTO <mehdi.aouir1@gmail.com>
2023-03-30 18:06:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,181 @@
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+
+
+//  1. Create two variables, myAge and humanDogRatio
+//  2. Multiply the two together and store the result in myDogAge
+//  3. Log myDogAge to the console
+
+//  let myAge = 20
+//  let humanDogRatio = 7
+
+//  let myDogAge = myAge * humanDogRatio
+
+//  console.log(myDogAge)
+
+
+
+// let count = 5
+
+// count = count + 1
+
+// count = count + 10
+
+// console.log(count)
+
+
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+
+function increment() {
+    console.log("The button was clicked")
+}
+
+
+
+// function coutdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// coutdown()
+
+// coutdown()
+
+
+// function myLogger() {
+//     console.log(42)
+// }
+// myLogger()
+
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+// logLapTime()
+
+
+
+//  let lapsCompleted = 0
+//  function incrementLap() {
+//      lapsCompleted = lapsCompleted + 1
+//  }
+//  incrementLap()
+//  incrementLap()
+//  incrementLap()
+//  console.log(lapsCompleted)
+
+
+
+// let countEl = document.getElementById("count-el")
+// console.log(countEl)
+// let count = 0
+// function increment(){
+//     count = count + 1
+//     countEl.innerText = count
+// }
+
+
+
+//  let countEl = document.getElementById("count-el")
+//  let count = 0
+//  function increment() {
+//      count = count + 1
+//      countEl.innerText = count
+//  }
+
+
+// function save(){
+//     console.log(count)
+// }
+
+// save()
+
+
+// let username = "per"
+
+// let message = "You have 3 new notifications"
+
+// let messageToUser = message + ", " + username + "!"
+// console.log(messageToUser)
+
+
+// let nickname = "Luky"
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + nickname
+// console.log(myGreeting)
+
+// let points = 4
+// let bonusPoints = 5
+// let totalPoints = points + bonusPoints
+// console.log(4 + 5) // 9
+
+// let points = "2"
+// let bonusPoints = "4"
+// let totalPoints = points + bonusPoints
+// console.log("2" + "4") // "24"
+
+// let points = "5"
+// let bonusPoints = 1
+// let totalPoints = points + bonusPoints
+// console.log("5" + 1) // "51"
+
+// let points = 100
+// let bonusPoints = "100"
+// let totalPoints = points + bonusPoints
+// console.log(100 + "100") // "100100"
+
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// let nickname = "Per Harald Borgen"
+// let greeting = "Welcome back "
+
+// welcomeEl.innerText = greeting + nickname
+
+// welcomeEl.innerText += "👋"
+
+
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// console.log(saveEl)
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+//     countEl.textContent = 0
+//     count = 0
+// }
